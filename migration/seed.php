083f0921f69e267e77dfbba0a225949fc64349a4
Fukuball Lin <fukuball@gmail.com>
2013-12-27 20:27:04 +0800

    debugging

--- migration/seed.php
+++ migration/seed.php
@@ -12,47 +12,55 @@
  * @link     http://www.fukuball.com
  */
 
-require dirname(dirname(__FILE__))."/config/db-param.php";
-
-$db_host       = @$database_server['matser']['db_host'];
-$db_user       = @$database_server['matser']['db_user'];
-$db_password   = @$database_server['matser']['db_password'];
-
-$con = new mysqli($db_host, $db_user, $db_password, "fukuphp", 3306);
-
-// Check connection
-if (mysqli_connect_errno()) {
-
-   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-   exit;
-
-} 
-
-$db_selected = mysqli_select_db($con, "fukuphp");
-
-if (!$db_selected) {
-
-   // If we couldn't, then it either doesn't exist, or we can't see it.
-   $sql = 'CREATE DATABASE fukuphp';
-
-   if (mysql_query($sql, $link)) {
-      echo "Database fukuphp created successfully\n";
-   } else {
-      echo 'Error creating database: ' . mysql_error() . "\n";
-   }
-
+$mysqli = new mysqli("fukuball-db1.cm4k6347dhx2.us-west-2.rds.amazonaws.com", "fukuphp", "pass2fuku", "fukuphp");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+echo $mysqli->host_info . "\n";
 
-//user_token
-$sql = "CREATE TABLE IF NOT EXISTS `user` (".
-        "`id` int(11) unsigned NOT NULL AUTO_INCREMENT,".
-        "`username` char(30) NOT NULL,".
-        "`is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',".
-        "`create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
-        "`modity_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
-        "`delete_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
-        "PRIMARY KEY (`id`)".
-      ") ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
-
-mysqli_query($con, $sql);
+//require dirname(dirname(__FILE__))."/config/db-param.php";
+//
+//print_r($database_server);
+//
+//$db_host       = @$database_server['matser']['db_host'];
+//$db_user       = @$database_server['matser']['db_user'];
+//$db_password   = @$database_server['matser']['db_password'];
+//
+//$con = mysqli_connect($db_host, $db_user, $db_password);
+//
+//// Check connection
+//if (mysqli_connect_errno()) {
+//
+//   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+//   exit;
+//
+//} 
+//
+//$db_selected = mysqli_select_db($con, "fukuphp");
+//
+//if (!$db_selected) {
+//
+//   // If we couldn't, then it either doesn't exist, or we can't see it.
+//   $sql = 'CREATE DATABASE fukuphp';
+//
+//   if (mysql_query($sql, $link)) {
+//      echo "Database fukuphp created successfully\n";
+//   } else {
+//      echo 'Error creating database: ' . mysql_error() . "\n";
+//   }
+//
+//}
+//
+////user_token
+//$sql = "CREATE TABLE IF NOT EXISTS `user` (".
+//        "`id` int(11) unsigned NOT NULL AUTO_INCREMENT,".
+//        "`username` char(30) NOT NULL,".
+//        "`is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',".
+//        "`create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
+//        "`modity_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
+//        "`delete_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',".
+//        "PRIMARY KEY (`id`)".
+//      ") ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
+//
+//mysqli_query($con, $sql);
 ?>
