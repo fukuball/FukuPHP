5a2945f12f551318b74aa2728b2fd9de6e22b0a7
Fukuball Lin <fukuball@gmail.com>
2013-12-30 16:25:32 +0800

    add seed

--- migration/seed.php
+++ migration/seed.php
@@ -55,4 +55,9 @@ $sql = "CREATE TABLE IF NOT EXISTS `user` (".
       ") ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;";
 
 mysqli_query($con, $sql);
+
+$sql = "INSERT INTO `user` (`id`, `path`, `is_deleted`, `create_time`, `modify_time`, `delete_time`) VALUES".
+         "(1, 'fukuball', 0, '2013-12-30 00:00:00', '2013-12-30 16:12:18', '0000-00-00 00:00:00');";
+
+mysqli_query($con, $sql);
 ?>
