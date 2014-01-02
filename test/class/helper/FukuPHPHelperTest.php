157db5705f49ce1108e0a46478f92a2f37984425
Fukuball Lin <fukuball@gmail.com>
2014-01-02 20:21:12 +0800

    add test

--- test/class/helper/FukuPHPHelperTest.php
+++ test/class/helper/FukuPHPHelperTest.php
@@ -7,10 +7,9 @@ class FukuPHPHelperTest extends PHPUnit_Framework_TestCase
 
         $response = FukuPHPHelper::doGet("http://www.fukuball.com/ckip-client/record/1.json");
         $response_decode = json_decode($response, true);
-        print_r($response_decode);
         $this->assertEquals(
             $response_decode['paragraph'], 
-            "http://www.fukuball.com/ckip-client/record/1.json"
+            "獨立音樂需要大家一起來推廣，歡迎加入我們的行列！"
         );
 
     }
