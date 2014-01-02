54b3cbef21e7f149761bd7bc00b7b2474b9288e9
Fukuball Lin <fukuball@gmail.com>
2014-01-02 20:18:22 +0800

    add test

--- test/class/helper/FukuPHPHelperTest.php
+++ test/class/helper/FukuPHPHelperTest.php
@@ -5,8 +5,13 @@ class FukuPHPHelperTest extends PHPUnit_Framework_TestCase
     public function testDoGet()
     {
 
-        $response = FukuPHPHelper::doGet("http://www.google.com");
-        $this->assertEquals(true, true);
+        $response = FukuPHPHelper::doGet("http://www.fukuball.com/ckip-client/record/1.json");
+        $response_decode = json_decode($response, true);
+        print_r($response_decode);
+        $this->assertEquals(
+            $response_decode['paragraph'], 
+            "http://www.fukuball.com/ckip-client/record/1.json"
+        );
 
     }
 
