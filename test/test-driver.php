a994ea1ace540be6c1d553f3baea2ba20d8d6fa0
Fukuball Lin <fukuball@gmail.com>
2014-01-02 15:22:50 +0800

    Init project

---
+++ test/test-driver.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * test-driver.php
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  /test/
+ * @author   Fukuball Lin <fukuball@indievox.com>
+ * @license  iNDIEVOX Licence
+ * @version  GIT: <0.0.1>
+ * @link     http://www.indievox.com
+ */
+
+dirname(dirname(__FILE__)).'/class/FukuPHP.inc';
+?>
