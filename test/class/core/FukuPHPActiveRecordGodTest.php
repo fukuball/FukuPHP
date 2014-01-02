56ff8f68ba5f3d7e986dd968b2e67d4bd94924ec
Fukuball Lin <fukuball@gmail.com>
2014-01-02 15:51:32 +0800

    add test

---
+++ test/class/core/FukuPHPActiveRecordGodTest.php
@@ -0,0 +1,69 @@
+<?php
+class FukuPHPActiveRecordGodTest extends PHPUnit_Framework_TestCase
+{
+
+   public function testConstruct()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testDestruct()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testSetDBAccess()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetDBAccess()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testIsExist()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetMaxId()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testDeleteListCache()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testCreate()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testCreateWithTime()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+}
+?>
