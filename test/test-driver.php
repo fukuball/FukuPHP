d789865d0cf3d98ded0b8cd1d87dd66f62c7a93d
Fukuball Lin <fukuball@gmail.com>
2014-01-02 18:42:51 +0800

    debugging

--- test/test-driver.php
+++ test/test-driver.php
@@ -13,4 +13,12 @@
  */
 
 dirname(dirname(__FILE__)).'/class/FukuPHP.inc';
+
+function loader($class) {
+    $file = $class . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+spl_autoload_register('loader');
 ?>
