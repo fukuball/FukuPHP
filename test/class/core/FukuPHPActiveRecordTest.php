7e95716a6bef854bfc7d59bef84d4962b1ced86d
Fukuball Lin <fukuball@gmail.com>
2014-01-02 15:44:04 +0800

    Init project

---
+++ test/class/core/FukuPHPActiveRecordTest.php
@@ -0,0 +1,125 @@
+<?php
+class FukuPHPActiveRecordTest extends PHPUnit_Framework_TestCase
+{
+
+   public function testConstruct()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testDestruct()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetter()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testSetter()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testSetDBAccess()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetDBAccess()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testSetMemcacheAccess()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetMemcacheAccess()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testDeleteMyMemcache()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetTableName()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetId()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testGetIsDeleted()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testToJSON()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testUpdate()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testSave()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testDestroy()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+   public function testRecover()
+   {
+
+      $this->assertEquals(true, true);
+
+   }
+
+}
+?>
