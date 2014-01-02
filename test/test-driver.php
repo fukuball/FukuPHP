47b1b40f400209e96cd597cde9b1a664d440f747
Fukuball Lin <fukuball@gmail.com>
2014-01-02 19:14:36 +0800

    debugging

--- test/test-driver.php
+++ test/test-driver.php
@@ -14,33 +14,6 @@
 
 require_once dirname(dirname(__FILE__))."/config/app-setter.php";
 
-// core
-function core_loader($class_name) {
-    $file = dirname(dirname(__FILE__)).'/class/core/'.$class_name . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-}
-spl_autoload_register('core_loader');
-
-// helper
-function helper_loader($class_name) {
-    $file = dirname(dirname(__FILE__)).'/class/helper/'.$class_name . '.php';
-    if (file_exists($file)) {
-        require_once $file;
-    }
-}
-spl_autoload_register('helper_loader');
-
-// helper
-function model_loader($class_name) {
-    $file = dirname(dirname(__FILE__)).'/class/model/'.$class_name . '.php';
-    if (file_exists($file)) {
-        require_once $file;
-    }
-}
-spl_autoload_register('model_loader');
-
 function loader($class) {
     $file = $class . '.php';
     if (file_exists($file)) {
