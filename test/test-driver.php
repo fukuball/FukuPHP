1693658e681e922934566df00a5604ab2ba94735
Fukuball Lin <fukuball@gmail.com>
2014-01-02 19:11:11 +0800

    debugging

--- test/test-driver.php
+++ test/test-driver.php
@@ -12,7 +12,32 @@
  * @link     http://www.indievox.com
  */
 
-dirname(dirname(__FILE__)).'/class/FukuPHP.inc';
+// core
+function core_loader($class_name) {
+    $file = dirname(dirname(__FILE__)).'/class/core/'.$class_name . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+}
+spl_autoload_register('core_loader');
+
+// helper
+function helper_loader($class_name) {
+    $file = dirname(dirname(__FILE__)).'/class/helper/'.$class_name . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+spl_autoload_register('helper_loader');
+
+// helper
+function model_loader($class_name) {
+    $file = dirname(dirname(__FILE__)).'/class/model/'.$class_name . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+}
+spl_autoload_register('model_loader');
 
 function loader($class) {
     $file = $class . '.php';
