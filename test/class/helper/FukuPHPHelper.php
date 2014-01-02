6d095fe4e2d7a7a04683c96ea76f983b410c2828
Fukuball Lin <fukuball@gmail.com>
2014-01-02 19:04:17 +0800

    debugging

--- test/class/helper/FukuPHPHelper.php
+++ test/class/helper/FukuPHPHelper.php
@@ -6,6 +6,7 @@ class FukuPHPHelperTest extends PHPUnit_Framework_TestCase
     {
 
         $response = FukuPHPHelper::doGet("http://www.google.com");
+        echo "test \n";
         echo $response;
         $this->assertEquals(true, true);
 
