b97f9ee74177c8211a10c734486f0a4ca7b454a7
Fukuball Lin <fukuball@gmail.com>
2014-01-03 18:58:05 +0800

    debugging

--- class/core/FukuPHPErrorMessenger.php
+++ class/core/FukuPHPErrorMessenger.php
@@ -48,7 +48,7 @@ class FukuPHPErrorMessenger
      *
      * @return void
      */
-    public function __construct($type,$parameter)
+    public function __construct($type, $parameter)
     {
  
         try {
--- test/view-controller/HomeViewControllerTest.php
+++ test/view-controller/HomeViewControllerTest.php
@@ -5,14 +5,15 @@ class HomeViewControllerTest extends PHPUnit_Framework_TestCase
     public function testRestPost()
     {
 
-        $home_view_controller = new HomeViewController();
-        ob_start();
-        $home_view_controller->restPost(array(), '/');
-        $output_content = ob_get_contents();
-        ob_end_clean();
-        $output_decode = json_decode($output_content, true);
-        $this->assertEquals('404', $output_decode['response']['status']['code']);
+        //$home_view_controller = new HomeViewController();
+        //ob_start();
+        //$home_view_controller->restPost(array(), '/');
+        //$output_content = ob_get_contents();
+        //ob_end_clean();
+        //$output_decode = json_decode($output_content, true);
+        //$this->assertEquals('404', $output_decode['response']['status']['code']);
 
+        throw new RuntimeException();
     }
 
     public function testRestGet()
