fa84138bf765126a9a2377157bb51b600fbeb90d
Fukuball Lin <fukuball@gmail.com>
2014-01-03 16:13:54 +0800

    add autoload

--- test/test-driver.php
+++ test/test-driver.php
@@ -14,6 +14,14 @@
 
 require_once dirname(dirname(__FILE__))."/config/test-app-setter.php";
 
+function view_loader($class_name) {
+    $file = dirname(dirname(__FILE__)).'/view-controller/'.$class_name . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+}
+spl_autoload_register('view_loader');
+
 function loader($class) {
     $file = $class . '.php';
     if (file_exists($file)) {
