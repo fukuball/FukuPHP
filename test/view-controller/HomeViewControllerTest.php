3c884e2403f26df443248511a1d0623c2863fb1f
fukuball <fukuball@gmail.com>
2014-01-04 00:42:00 +0800

    add HomeViewControllerTest

--- test/view-controller/HomeViewControllerTest.php
+++ test/view-controller/HomeViewControllerTest.php
@@ -26,6 +26,17 @@ class HomeViewControllerTest extends PHPUnit_Framework_TestCase
         $output_decode = json_decode($output_content, true);
         $this->assertEquals('404', $output_decode['response']['status']['code']);
 
+        $home_view_controller = new HomeViewController();
+        ob_start();
+        $home_view_controller->restGet(array(), '/');
+        $output_content = ob_get_contents();
+        ob_end_clean();
+        $thread_page_dom = new DomDocument();
+        @$thread_page_dom->loadHTML($output_content);
+        $title_tags = $thread_page_dom->getElementsByTagName('title');
+        $title = $title_tags->item(0)->nodeValue;
+        $this->assertEquals('Home Index Page', $title);
+
     }
 
     public function testRestPut()
