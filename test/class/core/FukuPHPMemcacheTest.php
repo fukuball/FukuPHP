720f01be4a1e2cf59aac00eea1b45162f579b483
fukuball <fukuball@gmail.com>
2014-01-04 17:40:57 +0800

    add test

---
+++ test/class/core/FukuPHPMemcacheTest.php
@@ -0,0 +1,22 @@
+<?php
+class FukuPHPMemcacheTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testConnectMemcache()
+    {
+
+        $memcache_obj = FukuPHPMemcache::connectMemcache('host1');
+        $this->assertEquals(true, is_a($memcache_obj, 'Memcache'));
+
+    }
+
+    public function testGetMemcacheKeys()
+    {
+
+        $memcache_keys = FukuPHPMemcache::getMemcacheKeys('host1');
+        $this->assertEquals(true, is_array ($memcache_keys));
+
+    }
+
+}
+?>
