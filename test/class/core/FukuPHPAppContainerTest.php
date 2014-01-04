4377e9be351ed79d23e3d13b9ae175bb37e76fa8
fukuball <fukuball@gmail.com>
2014-01-04 16:38:19 +0800

    add test

--- test/class/core/FukuPHPAppContainerTest.php
+++ test/class/core/FukuPHPAppContainerTest.php
@@ -2,6 +2,31 @@
 class FukuPHPAppContainerTest extends PHPUnit_Framework_TestCase
 {
 
+    ublic function setUp()
+    {
+
+        include SITE_ROOT."/migration/schema.php";
+
+        $db_obj = FukuPHPDBAccess::getInstance();
+        $param = array();
+        $query_instance = $db_obj->insertCommandPrepare($create_user_table_sql, $param);
+        $insert_id = $db_obj->insertCommandPrepare($insert_user_sql, $param);
+        unset($db_obj);
+
+    }
+
+    public function tearDown()
+    {
+
+        include SITE_ROOT."/migration/schema.php";
+
+        $db_obj = FukuPHPDBAccess::getInstance();
+        $param = array();
+        $query_instance = $db_obj->deleteCommandPrepare($drop_user_table_sql, $param);
+        unset($db_obj);
+
+    }
+
     public function testRun()
     {
 
@@ -18,8 +43,42 @@ class FukuPHPAppContainerTest extends PHPUnit_Framework_TestCase
         $title_tags = $thread_page_dom->getElementsByTagName('title');
         $title = $title_tags->item(0)->nodeValue;
         $this->assertEquals('Home Index Page', $title);
-        unset($view_controller);
+        unset($app_container);
 
+        $_SERVER['REQUEST_METHOD'] = 'NOT_EXIST';
+        $_SERVER['PATH_INFO'] = '/';
+
+        ob_start();
+        $app_container = new FukuPHPAppContainer();
+        $app_container->run();
+        $output_content = ob_get_contents();
+        ob_end_clean();
+        $output_decode = json_decode($output_content, true);
+        $this->assertEquals('404', $output_decode['response']['status']['code']);
+        unset($app_container);
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['PATH_INFO'] = '/fukuball';
+
+        ob_start();
+        $app_container = new FukuPHPAppContainer();
+        $app_container->run();
+        $output_content = ob_get_contents();
+        ob_end_clean();
+        $this->assertEquals('This is user fukuball', $output_content);
+        unset($app_container);
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['PATH_INFO'] = '/not-exit';
+
+        ob_start();
+        $app_container = new FukuPHPAppContainer();
+        $app_container->run();
+        $output_content = ob_get_contents();
+        ob_end_clean();
+        $output_decode = json_decode($output_content, true);
+        $this->assertEquals('404', $output_decode['response']['status']['code']);
+        unset($app_container);
 
     }
 
